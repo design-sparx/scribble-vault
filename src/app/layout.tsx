f2c4ddafaaf6f3d13425ceed1548b2663c287a61
Kelvin Kiptum Kiprop <kelvin.kiprop96@gmail.com>
2023-12-14 20:02:37 +0300

    chore: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import "@mantine/core/styles.css";
+
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
+
+export const metadata = {
+  title: "My Mantine app",
+  description: "I have followed setup instructions carefully",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body>
+        <MantineProvider>{children}</MantineProvider>
+      </body>
+    </html>
+  );
+}

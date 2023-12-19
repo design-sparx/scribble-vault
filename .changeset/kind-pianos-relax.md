910f7bd6d0eb1ce455a1a24d0054b823da35de3c
Kelvin Kiptum Kiprop <kelvin.kiprop96@gmail.com>
2023-12-19 21:17:47 +0300

    feat: added notes and page details pages

---
+++ .changeset/kind-pianos-relax.md
@@ -0,0 +1,5 @@
+---
+'scribble-vault': minor
+---
+
+feat: added notes and page details pages
--- src/types/workspace.ts
+++ src/types/workspace.ts
@@ -3,8 +3,8 @@ export type Workspace = {
   name: string;
   description: string;
   created_date: string;
-  modified_date: string
-  owner_name: string
-  owner_email: string
-  icon: string
+  modified_date: string;
+  owner_name: string;
+  owner_email: string;
+  icon: string;
 };
